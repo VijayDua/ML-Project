9a670b4118ebd92e660c091c3e4a45c370a54424
VijayDua <duavijay81@gmail.com>
2025-07-10 10:41:15 +0530

    post-mongoDB-account

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="VijayDua",
+    author_email="duavijay81@gmail.com",
+    packages=find_packages()
+)
